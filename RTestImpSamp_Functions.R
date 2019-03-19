7f4240d9436df5678d2473b5eb2de72ef983b0cd
Mauricio Olivares <maurice.olivares@gmail.com>
2019-03-19 15:56:19 -0500

    Monahan test  added

--- RTestImpSamp_Functions.R
+++ RTestImpSamp_Functions.R
@@ -1,5 +1,4 @@
 
-
 # -------------------------------------- #
 # A "Package" to test finite variance.
 # ---------------------------------------#
@@ -10,61 +9,9 @@
 # ---------------------------------------#
 
 
-# -------------------------------------- #
-# -------------------------------------- #
-# SECTION I: Generate different samples
-# for parametric families
-# -------------------------------------- #
-# -------------------------------------- #
-
-
-#' This generates data under a variety of specified DGPs indexed
-#' by "type"
-#' 
-#' Note: if te.sd == 0, we have constant treatment effect
-#' 
-gen.data <- function(N, te.mean = te.mean, te.sd = te.sd,
-                     type = c( "normal", "lognormal","t", "exponential" ) ){
-  
-  type = match.arg( type )
-  
-  # generate Y0
-  if(type == "normal"){
-    Y0 <- rnorm(N, 0, 1 )
-  }
-  else if(type == "lognormal"){
-    Y0 <- rlnorm(N, 0, 1/4 )
-  }
-  else if(type == "t"){
-    Y0 <- rt(N, df = 5 )
-  }
-  else if(type == "exponential"){
-    Y0 <- rexp(N,3)
-  } else {
-    stop( paste( "Unrecognized type '", type, "'", sep="" ) )
-  }
-  
-  # generate treatment and Y1
-  tau <- te.mean + te.sd * Y0
-  Y1 <- Y0 + tau
-  
-  # generate treatment: factor p is the [(p)*100]%
-  # of the sample that receives the treatment.
-  p <- .5
-  Tx = sample(N) <= p*(N)
-  
-  # generate Y obs
-  Y = ifelse( Tx, Y1, Y0 )
-  
-  data <- data.frame(Y = Y, Z = as.numeric(Tx))
-  
-  return(data)
-}
-
-
 # ---------------------------------------- #
 # ---------------------------------------- #
-# SECTION II: Estimate the shape parameter
+# SECTION I: Estimate the shape parameter
 # ---------------------------------------- #
 # ---------------------------------------- #
 
@@ -83,6 +30,18 @@ hill.est <- function(x,k){
 
 
 
+
+# ---------------------------------------- #
+# ---------------------------------------- #
+# SECTION II: Test Statistics
+# ---------------------------------------- #
+# ---------------------------------------- #
+
+
+monahan.test <- function(x,k){
+  t.stat <- sqrt(k/hill.est(x,k)^2)*(hill.est(x,k)-0.5)
+  return(t.stat)
+}
 
 
 
--- simulations_is.R
+++ simulations_is.R
@@ -34,3 +34,49 @@ functions<-list("Scripts/Perm_Test_library.R")
 lapply(functions, source)
 rm(functions)
 
+
+# -------------------------- #
+# Parameters of the DGP
+type <-  "normal"
+# Sample size. 
+N <- 200
+u <- 50
+
+
+
+
+# Parameters of the simulation 
+n.sims <- 200
+alpha <- 0.05
+n.perm <- 100
+n.tests <- 5 # Number of test statistics
+
+# Storage Matrix: 5 test statistics
+out <- matrix(NA,nrow=n.sims,ncol = n.tests)
+
+ptm0 <- proc.time()
+pb <- txtProgressBar(min = 0, max = n.sims, style = 3)
+
+for(i in 1:n.sims){
+  setTxtProgressBar(pb, i)
+  fake <- gen.data(N = N, te.mean = te.mean[j], te.sd = te.sd, type = type )
+  # Recentering
+    
+  # I) Observed Statistic
+  # Khmaladze transform based on Kolmogorov-Smirnov type test
+  KS.k <- Khm.trans(Y1.star,Y0.star,gdot0,t)
+    
+  # II) Permutations
+  Rn <- perm.dist.khm(Y1.star,Y0.star,KS.k,n.perm,t,gdot0)
+  # Rejection
+  out[i,j] <- reject.rule(Rn,alpha)
+    
+}
+close(pb)
+ptm1 <- proc.time()
+
+
+# Calculate rejection rates
+# Results
+rej.rates <-apply(out,2,function(x) mean(x))
+print(rej.rates)
